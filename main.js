2fee017a4a8d9b58da0707c06eb25ad49633fb62
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2022-12-15 19:14:03 +0100

    creato metodo 2

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 var app = new Vue({
 el: '#app',
 data: {
-
+    arrainfo:[],
+    arraigeneri:[],
 },
 mounted() {
+ axios.get('./api/index.php')
+ .then( (response) => {
+    this.arrainfo = response.data
+ });
 
+ axios.get('./api/arraifiltrato.php')
+ .then( (response) => {
+    this.arraigeneri = response.data
+    
+ })
 },
 beforeUpdate() {
 
